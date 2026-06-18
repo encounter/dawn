266c1cf8de969a364afa4fa49311631fc99a881e
Luke Street <luke@street.dev>
2026-06-17 21:08:27 -0600

    CI: Replace with encounter/dawn-build workflow

--- .github/workflows/dawn-ci.cmake
+++ .github/workflows/dawn-ci.cmake
@@ -1,52 +1,33 @@
-# This file caches variables which are platform specific.
-if (WIN32)
-    set(DAWN_USE_BUILT_DXC ON CACHE BOOL "")
-    set(CMAKE_SYSTEM_VERSION "$ENV{WIN10_SDK_VERSION}" CACHE STRING "")
-    set(CMAKE_WINDOWS_KITS_10_DIR "$ENV{WIN10_SDK_PATH}" CACHE STRING "")
-    set(CMAKE_VS_WINDOWS_TARGET_PLATFORM_VERSION "$ENV{WIN10_SDK_VERSION}" CACHE STRING "")
+# Release-build overrides for .github/workflows/release.yml.
+set(DAWN_FETCH_DEPENDENCIES ON CACHE BOOL "" FORCE)
+set(DAWN_ENABLE_INSTALL ON CACHE BOOL "" FORCE)
+set(DAWN_BUILD_SAMPLES OFF CACHE BOOL "" FORCE)
+set(DAWN_BUILD_BENCHMARKS OFF CACHE BOOL "" FORCE)
+set(DAWN_SUPPORTS_GLFW_FOR_WINDOWING OFF CACHE BOOL "" FORCE)
+set(DAWN_USE_GLFW OFF CACHE BOOL "" FORCE)
+set(TINT_BUILD_TESTS OFF CACHE BOOL "" FORCE)
+set(TINT_BUILD_CMD_TOOLS OFF CACHE BOOL "" FORCE)
+set(TINT_BUILD_IR_BINARY OFF CACHE BOOL "" FORCE)
+
+set(_system_name "${CMAKE_HOST_SYSTEM_NAME}")
+if (DEFINED CMAKE_SYSTEM_NAME AND NOT CMAKE_SYSTEM_NAME STREQUAL "")
+  set(_system_name "${CMAKE_SYSTEM_NAME}")
 endif ()
-set(DAWN_FETCH_DEPENDENCIES ON CACHE BOOL "")
-set(DAWN_ENABLE_INSTALL ON CACHE BOOL "")
-if (CMAKE_SYTEM_NAME STREQUAL "Linux")
-    # `sccache` seems effective only on linux.
-    # for windows, we could look into `buildcache`
-    # for macos, `sccache` causes an argument parse error for clang
-    # similar to https://github.com/fastbuild/fastbuild/issues/1041
-    # maybe we could use `ccache` in macos.
-    set(CMAKE_C_COMPILER_LAUNCHER "sccache" CACHE STRING "")
-    set(CMAKE_CXX_COMPILER_LAUNCHER "sccache" CACHE STRING "")
+
+if (_system_name STREQUAL "Windows")
+  set(DAWN_USE_BUILT_DXC ON CACHE BOOL "" FORCE)
+  set(CMAKE_SYSTEM_VERSION "$ENV{WIN10_SDK_VERSION}" CACHE STRING "" FORCE)
+  set(CMAKE_WINDOWS_KITS_10_DIR "$ENV{WIN10_SDK_PATH}" CACHE STRING "" FORCE)
+  set(CMAKE_VS_WINDOWS_TARGET_PLATFORM_VERSION "$ENV{WIN10_SDK_VERSION}" CACHE STRING "" FORCE)
+  set(CMAKE_MSVC_DEBUG_INFORMATION_FORMAT "Embedded" CACHE STRING "" FORCE)
+  set(CMAKE_POLICY_CMP0141 NEW CACHE STRING "" FORCE)
+  set(DAWN_BUILD_MONOLITHIC_LIBRARY SHARED CACHE STRING "" FORCE)
+elseif (_system_name STREQUAL "Linux")
+  set(CMAKE_C_COMPILER clang CACHE FILEPATH "" FORCE)
+  set(CMAKE_CXX_COMPILER clang++ CACHE FILEPATH "" FORCE)
+  set(CMAKE_LINKER_TYPE LLD CACHE STRING "" FORCE)
+  set(DAWN_USE_WAYLAND ON CACHE BOOL "" FORCE)
+elseif (_system_name MATCHES "^(Android|iOS)$")
+  set(DAWN_BUILD_MONOLITHIC_LIBRARY STATIC CACHE STRING "" FORCE)
+  set(BUILD_SHARED_LIBS OFF CACHE BOOL "" FORCE)
 endif ()
-# Mobile platform configuration
-# The workflow should set DAWN_MOBILE_BUILD=ON
-# for mobile platforms.
-if(DAWN_MOBILE_BUILD)
-    message(STATUS "Configuring Dawn Mobile Build")
-
-    # Build type
-    set(CMAKE_BUILD_TYPE "Release" CACHE STRING "")
-
-    # Disable samples and tests
-    set(BUILD_SAMPLES OFF CACHE BOOL "")
-    set(TINT_BUILD_TESTS OFF CACHE BOOL "")
-    set(TINT_BUILD_CMD_TOOLS OFF CACHE BOOL "")
-    set(TINT_BUILD_IR_BINARY OFF CACHE BOOL "")
-    set(DAWN_BUILD_SAMPLES OFF CACHE BOOL "")
-
-    # Use static monolithic library
-    set(DAWN_BUILD_MONOLITHIC_LIBRARY STATIC CACHE STRING "")
-    set(BUILD_SHARED_LIBS STATIC CACHE STRING BOOL "")
-
-    set(DAWN_USE_GLFW OFF CACHE BOOL "")
-    # Configure OpenGL variables by default.
-    # Enable OpenGL ES only for Android platform
-    set(DAWN_ENABLE_DESKTOP_GL OFF CACHE BOOL "")
-    message(STATUS "- DAWN_MOBILE_BUILD: ${DAWN_MOBILE_BUILD}")
-    if(DAWN_MOBILE_BUILD STREQUAL "android")
-        message(STATUS "- Enabling OpenGL ES for Android mobile build")
-        set(DAWN_ENABLE_OPENGLES ON CACHE BOOL "")
-    else()
-        message(STATUS "- Disabling OpenGL ES for non-Android mobile build")
-        set(DAWN_ENABLE_OPENGLES OFF CACHE BOOL "")
-    endif()
-
-endif()
